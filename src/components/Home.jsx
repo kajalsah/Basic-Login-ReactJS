8631e4f783a59b8595b2a22cb938e17243357850
Kajalsah <kajalksah07@gmail.com>
2021-09-25 02:50:07 +0530

    form completed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,25 +1,39 @@
 import React from "react";
+import  Register  from "./Register";
 
 function Home() {
+
+
+
+  
+
   return (
     <div className="home">
       <div class="container">
         <div class="row align-items-center my-5">
           <div class="col-lg-7">
-            <img
-              class="img-fluid rounded mb-4 mb-lg-0"
-              src="http://placehold.it/900x400"
-              alt=""
-            />
+            
           </div>
           <div class="col-lg-5">
-            <h1 class="font-weight-light">Home</h1>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
-            </p>
+            <h1 class="font-weight-light">LogIn</h1>
+
+            
+                <div className="form-group">
+                    <label>Email</label>
+                    <input type="email" className="form-control" placeholder="Enter email" />
+                </div>
+
+                <div className="form-group">
+                    <label>Password</label>
+                    <input type="password" className="form-control" placeholder="Enter password"/>
+                </div>
+
+                <button type="submit" className="btn btn-dark btn-lg btn-block">Login</button>
+                <div>
+                
+                    <b>Does not have an account?</b> <a href="#"  >Register</a> <b>here!</b>
+                
+                 </div>
           </div>
         </div>
       </div>
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,25 +1,38 @@
 import React from "react";
 
+
 function Register() {
   return (
     <div className="Register">
       <div class="container">
         <div class="row align-items-center my-5">
           <div class="col-lg-7">
-            <img
-              class="img-fluid rounded mb-4 mb-lg-0"
-              src="http://placehold.it/900x400"
-              alt=""
-            />
+            
           </div>
           <div class="col-lg-5">
             <h1 class="font-weight-light">Register</h1>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
-            </p>
+            <div className="form-group">
+                    <label>Name</label>
+                    <input type="text" className="form-control" placeholder="Enter Full Name" name="name"  />
+                </div>
+
+                <div className="form-group">
+                    <label>Email</label>
+                    <input type="email" className="form-control" placeholder="Enter email"  />
+                </div>
+
+                <div className="form-group">
+                    <label>Password</label>
+                    <input type="password" className="form-control" placeholder="Enter password"  />
+                </div>
+
+
+                <div className="form-group">
+                    <label>Phone No.</label>
+                    <input type="Phone" className="form-control" placeholder="Enter contact no"  />
+                </div>
+
+                <button type="submit" className="btn btn-dark btn-lg btn-block">Register</button>
           </div>
         </div>
       </div>
