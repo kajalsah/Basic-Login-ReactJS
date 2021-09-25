a046ccb9ec2c6bf5b4544e15408a7cff8bf9e8bd
Kajalsah <kajalksah07@gmail.com>
2021-09-26 00:16:20 +0530

    dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 
 function Dashboard() {
-  const [isLoggedIn, SetLoginState] = useState(false)
+  const [isLoggedIn, SetLoginState] = useState(false) ;
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
 if(isLoggedIn) {
 
@@ -12,7 +14,6 @@ if(isLoggedIn) {
             <div class="col-lg-5">
                <center><h1>Registered Users</h1></center> 
             </div>
-         
          </div>
 
   )
@@ -30,12 +31,12 @@ else {
             <h1 class="font-weight-light"><center><b>Admin Login</b></center></h1>
             <div className="form-group">
                     <label>Username</label>
-                    <input type="text" className="form-control" placeholder="Enter username" />
+                    <input type="text" className="form-control" placeholder="Enter username" value={username} onInput={e => setUsername(e.target.vale)} />
                 </div>
 
                 <div className="form-group">
                     <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password"/>
+                    <input type="password" className="form-control" placeholder="Enter password" value={password} onInput={e => setPassword(e.target.vale)}/>
                 </div>
 
                 <button type="submit" onClick={()=>SetLoginState(true)} className="btn btn-dark btn-lg btn-block">Login</button>
@@ -46,6 +47,8 @@ else {
     </div>
    );
  }
+
+
 }
 
 export default Dashboard;
